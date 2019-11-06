7b7a11767e04b437965e86a696b67c96c5bff27e
jyj <yjjiangphysics@gmail.com>
2019-11-06 01:42:14 -0600

    deleted Customerlist and change /customerlist to /topic

--- src/App.js
+++ src/App.js
@@ -14,13 +14,14 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Science-Faith discussion</h1>
         </header>
           <Switch>
                 <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
+                  <Redirect to="/topic"/>
                 )}/>
-                 <Route exact path='/customerlist' component={Customers} />
+                 <Route exact path='/topic' component={Customers} />
           </Switch>
       </div>
     </Router>
